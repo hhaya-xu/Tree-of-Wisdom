e7c0929b43d43cf1c11dcb4a122ad1b4f0c6ba0c
hhaya-xu <1474657453@qq.com>
2026-05-31 23:31:38 +0800

    fix: folder-icon SVG去重width→14 + index日期更新 + 面包屑恢复到中栏beforeBody

--- quartz/components/Explorer.tsx
+++ quartz/components/Explorer.tsx
@@ -132,15 +132,15 @@ export default ((userOpts?: Partial<Options>) => {
             <div class="folder-container">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
+                width="14"
+                height="14"
                 viewBox="5 8 14 8"
                 fill="none"
                 stroke="currentColor"
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="folder-icon" width="14" height="14"
+                class="folder-icon"
               >
                 <polyline points="6 9 12 15 18 9"></polyline>
               </svg>
